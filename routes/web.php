4314bc3d951842a9a757d697b4f3f9206393ef2a
kevin <kevinkalenga10@gmail.com>
2025-12-04 04:00:53 +0100

    Frontend Setup - Mastering Frontend Template(Part-1)

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\FrontendController;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
